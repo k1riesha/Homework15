9c989194f9f8ba44a00b30aa043450a29ed18e02
k1riesha <kiriesha999@gmail.com>
2024-01-26 02:17:27 +0200

    Commit

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String phoneNumber;
+
+    public Person(String firstName, String lastName, String city, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String personInfo() {
+        String info = "Зателефонувати громадянинові " + firstName + " " + lastName +
+                " з міста " + city + " можна за номером " + phoneNumber + ".";
+        return info;
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Will", "Smith", "New York", "2936729462846");
+        Person person2 = new Person("Jackie", "Chan", "Shanghai", "12312412412");
+        Person person3 = new Person("Sherlock", "Holmes", "London", "37742123513");
+
+        System.out.println(person1.personInfo());
+        System.out.println(person2.personInfo());
+        System.out.println(person3.personInfo());
+    }
+
+}
